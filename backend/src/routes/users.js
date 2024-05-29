275eb060892f6c1ed3300589e82f1a861cb0a8e4
Ignacio Cunial <igsecu@hotmail.com>
2024-05-29 18:29:39 -0300

    Added PUT /account route

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -5,7 +5,12 @@ const { userAuthenticated } = require("../middleware/auth");
 
 const fileUpload = require("express-fileupload");
 
-const { createAccount, login, logout } = require("../controllers/users");
+const {
+  createAccount,
+  login,
+  logout,
+  updateAccount,
+} = require("../controllers/users");
 
 // Logout process
 router.post("/logout", userAuthenticated, logout);
@@ -13,5 +18,21 @@ router.post("/logout", userAuthenticated, logout);
 router.post("/login", login);
 // Create account
 router.post("/register", createAccount);
+// Update account
+router.put("/account", userAuthenticated, updateAccount);
+// Update user image
+/* router.put(
+    "/image",
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: `${__dirname}/../uploads`,
+    }),
+    userAuthenticated,
+    updateUserImage
+  ); */
+// Delete user image
+/* router.delete("/image", userAuthenticated, deleteUserImage); */
+// Delete user account
+/* router.delete("/account", userAuthenticated, deleteUserAccount); */
 
 module.exports = router;
